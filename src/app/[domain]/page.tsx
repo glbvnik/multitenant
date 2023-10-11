84521659cc0e06067eb8ce2464df1799271aa933
glbvnik <glbvnik@gmail.com>
2023-10-11 11:20:22 +0200

    Manifest

--- src/app/[domain]/page.tsx
+++ src/app/[domain]/page.tsx
@@ -2,6 +2,12 @@ export function generateStaticParams() {
     return [{domain: 'tenantexample.fun'}]
 }
 
+export async function generateMetadata({params}: { params: { domain: string } }) {
+    return {
+        manifest: `http://${params.domain}/static/manifest.webmanifest`,
+    }
+}
+
 const Page = ({params}: { params: { domain: string } }) => {
     return (
         <div style={{
